8589555124456a1acde8464ce72a910c3a4d39d6
Sentiments <qianqian1903@gmail.com>
2024-05-21 21:31:57 +0800

    initial-commit

---
+++ app/routes/about.tsx
@@ -0,0 +1,40 @@
+import { HeadersFunction } from "@remix-run/node";
+import { json, useLoaderData } from "@remix-run/react";
+import axios from "axios";
+
+interface User {
+	firstName: string;
+	lastName: string;
+	_id: string;
+}
+
+export const loader = async () => {
+	const data = await axios.get<{
+		data: User[];
+	}>("https://remix-knowledge-sharing-backend.onrender.com/todos");
+
+	return json(data.data);
+};
+
+export const headers: HeadersFunction = () => {
+	return {
+		"Cache-Control": "public, s-maxage=60",
+	};
+};
+
+const AboutPage = () => {
+	const data = useLoaderData<typeof loader>();
+
+	return (
+		<div>
+			<h1>About Page</h1>
+			{data.data.map((item) => (
+				<div key={item._id}>
+					{item.firstName} {item.lastName}
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default AboutPage;
